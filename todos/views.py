6c7184e1a9feba08fb1e4c651612012015d8e412
Vinícius Daniel <VinciusDanie@123.com>
2024-11-19 19:29:16 -0300

    Questão 2 - (2,33 pts) Congure o ambiente para a ação de listagem de tarefas: ○ Passos: Crie o template, a view e demais congurações necessárias.

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from .models import Todo
 
 # Create your views here.
+class TodoListView(ListView):
+    model = Todo
+
